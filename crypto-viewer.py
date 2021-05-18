fa29f65579a11a40cd0332b665b83a15f5f8a751
mayursrt <mayurmachhi19@gmail.com>
2021-05-18 22:22:25 +0530

    refined column names

--- crypto-viewer.py
+++ crypto-viewer.py
@@ -23,10 +23,10 @@ This app retrieves cryptocurrency prices for the top 100 cryptocurrency from the
 
 # Page layout
 sid = st.sidebar # Sidebar
-col1, col2 = st.beta_columns((2,1)) # Main page
+# st, st = st.beta_columns((2,1)) # Main page
 
 
-# currency_price_unit = col1.selectbox('Select currency for price', ('USD', 'btc', 'eth')
+# currency_price_unit = st.selectbox('Select currency for price', ('USD', 'btc', 'eth')
 
 
 df = scrape_data()
@@ -38,67 +38,86 @@ df_selected_coin = df[ (df['coin_symbol'].isin(selected_coin)) ] # Filtering dat
 
 ## Sidebar - Number of coins to display
 num_coin = sid.slider('Display Top N Coins', 1, 100, 50)
+
 df_coins = df_selected_coin[:num_coin]
 df_coins.drop(['percent_change_1h', 'percent_change_24h','percent_change_7d'], axis=1, inplace=True)
 df_coins.rename({'rank' : '#', 'coin_name' : 'Name', 'coin_symbol' : 'Symbol', 'price' : 'Price',
 					'market_cap' : 'Market Cap', 'volume_24h' : 'Volume (24h)'},axis=1, inplace=True)
 
-col1.subheader('Price Data of Selected Cryptocurrency')
-col1.write('Data Dimension: ' + str(df_selected_coin.shape[0]) + ' rows and ' + str(df_selected_coin.shape[1]) + ' columns.')
+st.subheader('Price Data of Selected Cryptocurrency')
+st.write('Data Dimension: ' + str(df_selected_coin.shape[0]) + ' rows and ' + str(df_selected_coin.shape[1]) + ' columns.')
+df_coins = df_coins.set_index('#')
+#---------------------------------------------------------------------------------------------------
+# Coins Dataframe
+st.dataframe(df_coins)
+#---------------------------------------------------------------------------------------------------
 
-col1.dataframe(df_coins.assign(idx='').set_index('idx'))
-
-# col1.dataframe(df.assign(idx='').set_index('idx'))
-
-col1.markdown(filedownload(df_selected_coin), unsafe_allow_html=True)
+st.markdown(filedownload(df_selected_coin), unsafe_allow_html=True)
 
 # Preparing data for Bar plot of % Price change
-col1.subheader('Table of % Price Change')
+
 df_change = pd.concat([df.coin_name, df.coin_symbol, df.percent_change_1h, df.percent_change_24h, df.percent_change_7d], axis=1)
-df_change = df_change.set_index('coin_symbol')
+df_change.rename({'coin_symbol' : 'Coin Symbol'}, axis=1, inplace=True)
+df_change = df_change.set_index('Coin Symbol')
 df_change['positive_percent_change_1h'] = df_change['percent_change_1h'] > 0
 df_change['positive_percent_change_24h'] = df_change['percent_change_24h'] > 0
 df_change['positive_percent_change_7d'] = df_change['percent_change_7d'] > 0
-
-df_change.rename({'coin_name' : 'Name', 'coin_symbol' : 'Symbol', 'percent_change_1h' : '% Change (1h)', 
-					'percent_change_24h' : '% Change (24h)', 'percent_change_7d' : '% Change (7d)'})
-col1.dataframe(df_change)
+df_change = df_change[:num_coin]
+df_change_show = df_change.drop(['positive_percent_change_7d', 'positive_percent_change_24h', 'positive_percent_change_1h'], axis=1)
+df_change_show.rename({'coin_name' : 'Name', 'coin_symbol' : 'Symbol', 'percent_change_1h' : 'Percent Change (1h)', 
+					'percent_change_24h' : 'Percent Change (24h)', 'percent_change_7d' : 'Percent Change (7d)'}, axis=1, inplace=True)
 
 # Conditional creation of Bar plot (time frame)
-col2.subheader('Bar plot of % Price Change')
+st.subheader('Bar plot of % Price Change')
 
-col3, col4 = st.beta_columns((1,1))
 ## Sidebar - Percent change timeframe
-percent_timeframe = col2.selectbox('Percent change time frame',
+col1, col2, col3 = st.beta_columns((2,2,5))
+percent_timeframe = col1.selectbox('Percent change time frame',
                                     ['7d','24h', '1h'])
+col3.empty()
+
 percent_dict = {"7d":'percent_change_7d',"24h":'percent_change_24h',"1h":'percent_change_1h'}
 selected_percent_timeframe = percent_dict[percent_timeframe]
 
 ## Sidebar - Sorting values
 sort_values = col2.selectbox('Sort values?', ['Yes', 'No'])
 
-
+#---------------------------------------------------------------------------------------------------
+# % Change Bar Plot
 if percent_timeframe == '7d':
     if sort_values == 'Yes':
         df_change = df_change.sort_values(by=['percent_change_7d'])
-    col2.write('*7 days period*')
-    plt.figure(figsize=(5,25))
+    st.write('*7 days period*')
+    plt.figure(figsize=(25,5))
     plt.subplots_adjust(top = 1, bottom = 0)
-    df_change['percent_change_7d'].plot(kind='barh', color=df_change.positive_percent_change_7d.map({True: 'g', False: 'r'}))
-    col2.pyplot(plt)
+    plt.xlabel('Coin Symbol')
+    plt.ylabel('Price')
+    df_change['percent_change_7d'].plot(kind='bar', color=df_change.positive_percent_change_7d.map({True: 'g', False: 'r'}))
+    st.pyplot(plt)
 elif percent_timeframe == '24h':
     if sort_values == 'Yes':
         df_change = df_change.sort_values(by=['percent_change_24h'])
-    col2.write('*24 hour period*')
-    plt.figure(figsize=(5,25))
+    st.write('*24 hour period*')
+    plt.figure(figsize=(25,5))
     plt.subplots_adjust(top = 1, bottom = 0)
-    df_change['percent_change_24h'].plot(kind='barh', color=df_change.positive_percent_change_24h.map({True: 'g', False: 'r'}))
-    col2.pyplot(plt)
+    plt.xlabel('Coin Symbol')
+    plt.ylabel('Price')
+    df_change['percent_change_24h'].plot(kind='bar', color=df_change.positive_percent_change_24h.map({True: 'g', False: 'r'}))
+    st.pyplot(plt)
 else:
     if sort_values == 'Yes':
         df_change = df_change.sort_values(by=['percent_change_1h'])
-    col2.write('*1 hour period*')
-    plt.figure(figsize=(5,25))
+    st.write('*1 hour period*')
+    plt.figure(figsize=(25,5))
     plt.subplots_adjust(top = 1, bottom = 0)
-    df_change['percent_change_1h'].plot(kind='barh', color=df_change.positive_percent_change_1h.map({True: 'g', False: 'r'}))
-    col2.pyplot(plt)
+    plt.xlabel('Coin Symbol')
+    plt.ylabel('Price')
+    df_change['percent_change_1h'].plot(kind='bar', color=df_change.positive_percent_change_1h.map({True: 'g', False: 'r'}))
+    st.pyplot(plt)
+#---------------------------------------------------------------------------------------------------
+
+st.subheader('Table of % Price Change')
+#---------------------------------------------------------------------------------------------------
+# Change Dataframe
+st.dataframe(df_change_show)
+#---------------------------------------------------------------------------------------------------
